4f0994867185043676d6229da35ad3e6beb9b273
Lige <lige.williamson@gmail.com>
2019-02-08 21:13:41 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -17,13 +17,13 @@ def greeting(greeting, name)
 end
 
 def return_a_value
-"Nice"
+  "Nice"
 end
 
 def last_evaluated_value
-"expert"
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
